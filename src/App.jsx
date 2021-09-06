5f51e2046ae366b988b85a75164640801fb8f0ef
Tugay Demirel <tugaydemirelyedek@gmail.com>
2021-09-06 08:47:42 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import Cart from "./pages/Cart";
+import Homepage from "./pages/Homepage";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
+
+const App = () => {
+  return (
+    <>
+    <Register />
+    <Login />
+    <Cart />
+    <Homepage />
+    <Product />
+    <ProductList />
+    </>
+  )
+  ;
+};
+
+export default App;
